0fb6162874b1fc55e1c35aaafaf9c25d8174a163
Adriano Galvão <adrianogalvao@agbtecnologia.com.br>
2019-07-23 21:08:28 -0300

    Adicionada a classe Posicao.

---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XadrezConsole.Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Conluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Conluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Conluna;
+        }
+    }
+}
